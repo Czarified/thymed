dfe620574b6aa9e75af7a8c1f745f11e7b86bd24
Benjamin Crews <aceF22@gmail.com>
2022-09-18 11:46:55 -0400

    CLI Work
    Swapped CLI to be a command-base. So Thymed will be the base entry point, with multiple command options.
    
    Added an informational option with "hello."
    
    Updated default_code to provide user-friendly feedback.
    
    Updated punch to take an optional charge code id.
    
    Added NEW command to create new charge codes with an interactive prompt. Information must be confirmed before being written. Currently there is no CLI command to remove charge codes from the database.
    
    Added NEW command to list all charge codes and their current active state.

--- src/thymed/__main__.py
+++ src/thymed/__main__.py
@@ -3,7 +3,13 @@ import importlib.metadata
 import json
 
 import click
+
 from rich.console import Console
+from rich.panel import Panel
+from rich.prompt import Prompt, IntPrompt, Confirm
+from rich.table import Table
+from rich.traceback import install
+install()
 
 import thymed
 
@@ -11,14 +17,22 @@ import thymed
 __version__ = importlib.metadata.version("thymed")
 
 
-@click.command()
+@click.group()
 @click.version_option()
 def main() -> None:
     """Thymed.
 
     This function will be used to execute the TUI.
     For now, it's just a placeholder.
+
+    For more information, try: thymed hello
     """
+    pass
+    
+
+@main.command()
+def hello():
+    """See information about Thymed."""
     console = Console()
     console.print("Hello World!\n")
     console.print(
@@ -41,28 +55,129 @@ def default_code() -> thymed.ChargeCode:
     with open(thymed._CHARGES) as f:
         codes = json.load(f, object_hook=thymed.object_decoder)
 
-    default_id = thymed.__OPTIONS["database"]["default"]
+    try:
+        default_id = thymed.__OPTIONS["database"]["default"]
+        return codes[str(default_id)]
+    except KeyError:
+        # KeyError means default code isn't set. We should notify user,
+        # and exit.
+        console = Console()
+        text = (
+            "Looks like you haven't set a default ChargeCode, "
+            "or the code provided isn't in the database somehow. "
+            "Try providing the specific code you want to punch in, "
+            "or set the default code for future with: `thymed set default <id>`"
+        )
+        console.print(Panel(text, title='[magenta]No Default Found', width=100, style='spring_green1'))
 
-    return codes[str(default_id)]
 
+@main.command()
+@click.argument('id', nargs=-1)
+def punch(id) -> None:
+    """Punch a ChargeCode.
 
-@click.command()
-def punch_default() -> None:
-    """Punch the default ChargeCode.
-
-    Grab the default code, and call its `punch` method.
+    Punch the ChargeCode id provided.
+    
+    If no id provided, grab the default code, 
+    and call its `punch` method.
+    
     Punches the current time. Write the data and the code,
     then exit.
     """
+    # Manually check some argument varialbes...
+    if len(id)>1:
+        raise NotImplementedError
+    elif len(id)==1:
+        id = id[0]
+
     console = Console()
     console.print("[spring_green3 italic]Thymed.[/]\n")
-    code = default_code()
+    if not id:
+        code = default_code()
+    else:
+        with open(thymed._CHARGES) as f:
+            try:
+                codes = json.load(f, object_hook=thymed.object_decoder)
+            except json.JSONDecodeError:
+                # If the file is completely blank, we will get an error
+                codes = dict()
+        try:
+            code = codes[id]
+        except KeyError:
+            text = (
+            f"Cannot find the charge code with id: {id}\n"
+            "Try viewing available charge codes with `thymed list` or "
+            "try punching the default code with `thymed punch`"
+            )
+            console.print(Panel(text, title='[magenta]Code Not Found', width=100, style='spring_green1'))
+            code = None
+    if code == None:
+        # code is None if the default code was not found, nor was it provided
+        console.print("No code to punch, so exiting...")
+        return None
+
     console.print(f"Punching Charge Code: [medium_spring_green]{code.name}")
     code.punch()
     code.write_class()
     code.write_json()
     console.print("[spring_green3 italic]   ...Done!")
 
+
+@main.command()
+def create():
+    """Create a new ChargeCode.
+
+    Create a new ChargeCode object with the given id.
+    """
+    console = Console()
+    name = Prompt.ask("Enter a name for the ChargeCode")
+    description = Prompt.ask("Description for this ChargeCode")
+    id = IntPrompt.ask("A unique identifier for this code (integer value)")
+
+    new_code = thymed.ChargeCode(name, description, id)
+    console.print(new_code)
+    confirmed = Confirm.ask("Confirm ChargeCode data? (If no, you will exit with no changes)")
+
+    if confirmed:
+        new_code.write_class()
+        # TODO: Make this prettier
+        console.print("Wrote ChargeCode data to the Thymed Charges file!")
+    else:
+        # TODO: Make this prettier
+        console.print("Exiting without writing new ChargeCode...Try again.")
+
+
+@main.command()
+def list():
+    """List out all the available charge codes.
+    """
+    with open(thymed._CHARGES) as f:
+        try:
+            codes = json.load(f, object_hook=thymed.object_decoder)
+
+            # Sort the codes dictionary by key (code id)
+            sorted_codes = sorted(codes.items(), key=lambda kv: int(kv[0]))
+            codes = [x[1] for x in sorted_codes]
+        except json.JSONDecodeError:
+            # If the file is completely blank, we will get an error
+            codes = dict()
+
+    console = Console()
+    table = Table(title="ChargeCodes in Current Database", style="spring_green2")
+    table.add_column("ID", justify="right", style="green", no_wrap=True)
+    table.add_column("NAME", style="spring_green3 italic")
+    table.add_column("DESCRIPTION", style="spring_green4")
+    table.add_column("ACTIVE", style="green")
+
+    for code in codes:
+        table.add_row(
+            str(code.id), 
+            code.name, 
+            code.description,
+            str(code.is_active)
+        )
+
+    console.print(table)
 
 if __name__ == "__main__":
     main(prog_name="thymed")  # pragma: no cover
